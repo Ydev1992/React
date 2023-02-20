2528d6da89d1c71cab0a882711b4555b29069aa7
Olaf119 <flyn.rider0x@gmail.com>
2023-02-20 12:23:26 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = 'Landing'>
+        <h1>My React Testing Page</h1>
+        <p className = 'p-description'>Today I've joined Internet. And also get my first task about React so here I am.</p>
+        <p className = 'p-description'>Learn about React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
