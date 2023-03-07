4df4cfa054e90c7035ee8c253c58a67fb4c649fc
olaf119 <flyn.rider0x@gmail.com>
2023-03-07 09:49:37 -0500

    initial commit 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,126 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const process = require("process");
+const { exec } = require("child_process");
+const util = require("util");
+const { existsSync } = require("fs");
+const execAsync = util.promisify(exec);
+const {
+  parse,
+  addDays,
+  addYears,
+  isWeekend,
+  setHours,
+  setMinutes,
+  setSeconds
+} = require("date-fns");
+const chalk = require("chalk");
+const ora = require("ora");
+const boxen = require("boxen");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+module.exports = function({ commitsPerDay, workdaysOnly, startDate, endDate }) {
+  const commitDateList = createCommitDateList({
+    workdaysOnly,
+//    commitsPerDay: commitsPerDay.split(","),
+    commitsPerDay: "0, 2".split(","),
+//    startDate: startDate ? parse(startDate) : addYears(new Date(), -1),
+    startDate: "2017/01/01" ? parse("2017/01/01") : addYears(new Date(), -1),
+    endDate: "2022/12/31" ? parse("2022/12/31") : new Date()
+//  endDate: endDate ? parse(endDate) : new Date()
+  });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  (async function() {
+    const historyFolder = "my-history";
+
+    // Remove git history folder in case if it already exists.]
+    if (existsSync(`./${historyFolder}`)) {
+      await execAsync(
+        `${
+          process.platform === "win32" ? "rmdir /s /q" : "rm -rf"
+        } ${historyFolder}`
+      );
+    }
+
+    // Create git history folder.
+    await execAsync(`mkdir ${historyFolder}`);
+    process.chdir(historyFolder);
+    await execAsync(`git init`);
+
+    // Create commits.
+    for (const date of commitDateList) {
+      // Change spinner so user can get the progress right now.
+      const dateFormatted = new Intl.DateTimeFormat("en", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+      }).format(date);
+      console.log('------------------data formatted', dateFormatted);
+      //spinner.text = `Generating your Github activity... (${dateFormatted})\n`;
+
+      await execAsync(`echo "${date}" > foo.txt`);
+      await execAsync(`git add .`);
+      await execAsync(`git config user.name Olaf119`);
+      await execAsync(`git config user.email flyn.rider0x@gmail.com`);
+      await execAsync(`git commit --quiet --date "${date}" -m "fake commit"`);
+    }
+
+//    spinner.succeed();
+
+    console.log(
+      boxen(
+        `${chalk.green("Success")} ${
+          commitDateList.length
+        } commits have been created.
+      If you rely on this tool, please consider buying me a cup of coffee. I would appreciate it 
+      ${chalk.blueBright("https://www.buymeacoffee.com/artiebits")}`,
+        { borderColor: "yellow", padding: 1, align: "center" }
+      )
+    );
+  })();
+};
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createCommitDateList({
+  commitsPerDay,
+  workdaysOnly,
+  startDate,
+  endDate
+}) {
+
+  const commitDateList = [];
+  let currentDate = startDate;
+
+  while (currentDate <= endDate) {
+    if (workdaysOnly && isWeekend(currentDate)) {  
+      currentDate = addDays(currentDate, 1);
+      continue;
+    }
+
+    var isAddDay = Math.floor(Math.random() * 1000) % 3;
+
+    if(isAddDay != 1){
+      currentDate = addDays(currentDate, 1);
+      continue;
+    }
+
+    for (let i = 0; i < (Math.floor(Math.random() * 1000)) % 4; i++) {
+      const dateWithHours = setHours(currentDate, getRandomIntInclusive(9, 16));
+      const dateWithHoursAndMinutes = setMinutes(
+        dateWithHours,
+        getRandomIntInclusive(0, 59)
+      );
+      const commitDate = setSeconds(
+        dateWithHoursAndMinutes,
+        getRandomIntInclusive(0, 59)
+      );
+
+      commitDateList.push(commitDate);
+    }
+    currentDate = addDays(currentDate, 1);
+  }
+
+  return commitDateList;
+}
